57fe018f712fa4fd7ec4d7fa3bfa7528a53cb8dd
Karan Singh <karansinghreen@gmail.com>
2020-12-22 16:52:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or 'karan-hello-world-server.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'karan-hello-world-db'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'udacityadmin'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'p@ssword1234'
+    SQL_SERVER = os.environ.get('SQL_SERVER') or 'SQL_SERVER'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'SQL_DATABASE'
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'SQL_USER_NAME'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'SQL_PASSWORD'
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'karanhelloworld12345'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'F1Hy+3Oj1za4lkKVChCM/AC5j969vkP6kSJgIt1EVqzADFNqzo2arzZa4cIISFn12BPFwgH2QlXxExIPZwLB2A=='
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'BLOB_ACCOUNT'
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'BLOB_STORAGE_KEY'
     BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
